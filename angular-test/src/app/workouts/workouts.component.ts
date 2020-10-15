98db02278b8a2b513aae9fb5b11256137bfe1fd9
jake <guswns1853@gmail.com>
2020-10-15 22:39:03 +0900

    apply typescript interface

--- angular-test/src/app/workouts/workouts.component.ts
+++ angular-test/src/app/workouts/workouts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Workout } from '../workout';
 
 @Component({
   selector: 'app-workouts',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./workouts.component.css'],
 })
 export class WorkoutsComponent implements OnInit {
-  workout = '데드리프트';
+  workout: Workout = {
+    id: 0,
+    name: '데드리프트',
+    count: 0,
+  };
   constructor() {}
   ngOnInit(): void {}
 }

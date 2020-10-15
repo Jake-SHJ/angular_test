7ffacdbfc17c66d7bb606ce95fc4a2cd3b13f08e
jake <guswns1853@gmail.com>
2020-10-15 22:32:39 +0900

    template data injection

--- angular-test/src/app/workouts/workouts.component.ts
+++ angular-test/src/app/workouts/workouts.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-workouts',
   templateUrl: './workouts.component.html',
-  styleUrls: ['./workouts.component.css']
+  styleUrls: ['./workouts.component.css'],
 })
 export class WorkoutsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  workout = '데드리프트';
+  constructor() {}
+  ngOnInit(): void {}
 }

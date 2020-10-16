5427f6ec36c0a8e4233644812816809355f15792
jake <guswns1853@gmail.com>
2020-10-16 16:47:29 +0900

    add click states and control them

--- angular-test/src/app/workouts/workouts.component.ts
+++ angular-test/src/app/workouts/workouts.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { WORKOUTS } from '../mock-workouts';
+import { Workout } from '../workout';
 
 @Component({
   selector: 'app-workouts',
@@ -8,6 +9,10 @@ import { WORKOUTS } from '../mock-workouts';
 })
 export class WorkoutsComponent implements OnInit {
   workouts = WORKOUTS;
+  selectedWorkout: Workout;
   constructor() {}
   ngOnInit(): void {}
+  onSelect(workout: Workout): void {
+    this.selectedWorkout = workout;
+  }
 }

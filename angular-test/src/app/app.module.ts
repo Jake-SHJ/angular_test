d4b0fdd579f75ab00893a1cbcde502cc13d700fb
jake <guswns1853@gmail.com>
2020-10-16 16:22:12 +0900

    load FormsModule

--- angular-test/src/app/app.module.ts
+++ angular-test/src/app/app.module.ts
@@ -1,20 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WorkoutsComponent } from './workouts/workouts.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    WorkoutsComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, WorkoutsComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

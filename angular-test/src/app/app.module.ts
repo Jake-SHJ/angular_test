4ef9a12d26e6fec3ac24aefddfab35ff765dc200
jake <guswns1853@gmail.com>
2020-10-16 16:53:54 +0900

    improve component structure - components separation

--- angular-test/src/app/app.module.ts
+++ angular-test/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WorkoutsComponent } from './workouts/workouts.component';
+import { WorkoutDetailComponent } from './workout-detail/workout-detail.component';
 
 @NgModule({
-  declarations: [AppComponent, WorkoutsComponent],
+  declarations: [AppComponent, WorkoutsComponent, WorkoutDetailComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ angular-test/src/app/workout-detail/workout-detail.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Workout } from '../workout';
+
+@Component({
+  selector: 'app-workout-detail',
+  templateUrl: './workout-detail.component.html',
+  styleUrls: ['./workout-detail.component.css'],
+})
+export class WorkoutDetailComponent implements OnInit {
+  @Input() workout: Workout;
+  constructor() {}
+
+  ngOnInit(): void {}
+}
